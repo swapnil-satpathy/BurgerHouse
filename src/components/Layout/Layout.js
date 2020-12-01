9edee97a5f99c62901297324bca25b1c611b4f5d
Swapnil <swapnilsatpathycse24@gmail.com>
2020-12-01 20:02:55 +0530

    Outputting Burger Components Dynamically

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../HOC/Aux.js';
+import styles from './Layout.module.css';
 const layout = (props) => {
   return (
     <Aux>
     <div>Toolbar,Sidedrawer,Backdrop </div>
-    <main>
+    <main className={styles.Content}>
         {props.children}
     </main>
     </Aux>
